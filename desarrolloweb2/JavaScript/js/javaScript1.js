244c081773c5fdf1ff8b871851f62a70d367ca41
Denilson19 <132634018+Denilson19@users.noreply.github.com>
2024-04-22 09:18:23 -0400

    Delete desarrolloweb2/JavaScript directory

--- desarrolloweb2/JavaScript/js/javaScript1.js
+++
@@ -1,8 +0,0 @@
-/*
-Descripcion: Ejemplo Hola mundo console.log
-Desarollador: Denilson Noel Ledezma Condori
-Fecha: 2024-04-15
-Cambios: NInguno
-*/ 
-console.log("Hola se mostrara el externo?");
-        
